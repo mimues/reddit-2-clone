fb959121f8395e8dfc0c160da7acd0942bbd28d4
Miguel Muñoz <mmestelles@gmail.com>
2022-08-29 12:42:37 +0200

    comment typings

--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -101,16 +101,16 @@ function PostPage() {
 
       <div className="rounded-b-md border border-t-0 border-l-0 border-r-0 border-gray-300 bg-white p-5 pl-16 pt-0">
         <hr className="py-1" />
-        
+
 
         {post?.comments
-          // ?.map((comment) => {
-          //   return {
-          //     ...comment,
-          //     created_at: new Date(comment?.created_at).getTime(),
-          //   };
-          // })
-          // .sort((a, b) => b.created_at - a.created_at)
+          ?.map((comment: Comment) => {
+            return {
+              ...comment,
+              created_at: new Date(comment?.created_at).getTime(),
+            };
+          })
+          .sort((a, b) => b.created_at - a.created_at)
           ?.map((comment) => {
             return (
               <div
