4d7b7401d8f05d3afb2328e1b7bab45cf428d5e5
Miguel Muñoz <mmestelles@gmail.com>
2022-08-29 13:40:16 +0200

    comment operators

--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -117,19 +117,19 @@ function PostPage() {
             return (
               <div
                 className="relative flex items-center space-x-2 space-y-5"
-                key={comment.id}
+                key={comment?.id}
               >
                 <div className="z-50">
-                  <Avatar seed={comment.username} />
+                  <Avatar seed={comment?.username} />
                 </div>
                 <div className="">
                   <p className="py-2 text-xs text-gray-400">
                     <span className="font-semibold text-gray-600">
-                      {comment.username}
+                      {comment?.username}
                     </span>{" "}
-                    · <TimeAgo date={comment.created_at} />
+                    · <TimeAgo date={comment?.created_at} />
                   </p>
-                  <p>{comment.text}</p>
+                  <p>{comment?.text}</p>
                 </div>
                 <hr className="absolute top-10 left-3 z-0 h-16 border" />
               </div>
